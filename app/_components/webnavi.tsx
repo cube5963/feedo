7d979488f134b6c3e3d25b575309a7b1598af52c
Raito5963 <raito.5963.co@gmail.com>
2025-04-24 13:55:10 +0900

    sa

---
+++ app/_components/webnavi.tsx
@@ -0,0 +1,14 @@
+export default function Webnavi(){
+    return(
+        <nav>
+            <ul>
+                <li>Feedo</li>
+                <li>機能</li>
+                <li>活用例</li>
+                <li>料金とプラン</li>
+                <li><button>ログイン</button></li>
+                <li><button>登録</button></li>
+            </ul>
+        </nav>
+    );
+}
