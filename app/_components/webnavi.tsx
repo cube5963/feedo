dab89c9924aeb3128fb9e093b6d3ba471f130756
Raito5963 <181489458+Raito5963@users.noreply.github.com>
2025-06-05 04:11:56 +0000

    Webnaviコンポーネントにロゴを追加し、ボタンのスタイルを改善。サインインページのレイアウトを調整し、ユーザー名とメールアドレスの入力フィールドをスタイリング。

--- app/_components/webnavi.tsx
+++ app/_components/webnavi.tsx
@@ -1,17 +1,88 @@
 import {
+    autocompleteClasses,
     Button,
 }from "@mui/material";
+import Image from "next/image";
+import logo from "../../public/logo.png";
 
 export default function Webnavi(){
     return(
-        <div>
-            <ul>
-                <li><a href="/">FEEDO</a></li>
+        <div 
+        style={{
+            display: "flex",
+            justifyContent: "space-between", 
+            padding: "10px", 
+            margin: 0,
+            width: "auto",
+            backgroundColor: "#fff", 
+            borderBottom: "1px solid #ccc",
+        }}>
+            <ul 
+            style={{display: "flex", 
+            listStyleType: "none", 
+            padding: 0, 
+            margin: 0,
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+            fontSize: "20px",
+            fontWeight: "bold",
+            color: "#333",
+            }}>
+                <li>
+                    <a href="/">
+                    <Image 
+                    src={logo} 
+                    alt="Logo" 
+                    height={40} 
+                    width={250}
+                    style={{
+                        width: "auto",
+                        height: "auto",
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                        objectFit: "contain",
+                    }}/>
+                    </a>
+                </li>
                 <li>機能</li>
                 <li>活用例</li>
                 <li>プランと料金</li>
-                <li><Button href="/signin" variant="contained">サインイン</Button></li>
-                <li><Button href="/signup" variant="contained">登録</Button></li>
+                <li>
+                    <Button 
+                    href="/signin" 
+                    variant="contained"
+                    style={{
+                        backgroundColor: "#000",
+                        color: "#fff",
+                        padding: "10px 30px",
+                        borderRadius: "5px",
+                        textDecoration: "none",
+                        fontSize: "16px",
+                        fontWeight: "bold",
+                        minWidth: "120px",
+                    }}>
+                        サインイン
+                    </Button>
+                </li>
+                <li>
+                    <Button 
+                    href="/signup" 
+                    variant="contained"
+                    style={{
+                        backgroundColor: "#1976d2",
+                        color: "#fff",
+                        padding: "10px 30px",
+                        borderRadius: "5px",
+                        textDecoration: "none",
+                        fontSize: "16px",
+                        fontWeight: "bold",
+                        minWidth: "120px",
+                    }}
+                    >
+                        登録
+                    </Button>
+                </li>
             </ul>
         </div>
     );
