400a9638bc2235a28a3b560c13f88a27523007b9
Miranya1 <145127840+Miranya1@users.noreply.github.com>
2025-06-12 06:29:34 +0000

    soso

--- app/newproject/page.tsx
+++ app/newproject/page.tsx
@@ -1,14 +1,31 @@
+import { NoEncryption } from "@mui/icons-material";
 import {
     Button,
 } from "@mui/material";
 
 export default function NewProject(){
     return(
-        <div>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "20px",
+                margin: "20px auto",
+                marginTop:"30px",
+                width: "100%",
+                height: "100%",
+                backgroundColor: "#fff",
+            }}>
             <h1>新規作成</h1>
             <h3>AIを使用しますか？</h3>
-            <Button href="/setup" variant="contained">使用しません<span display="none">使用します</span></Button>
-            <Button variant="outlined" color="inherit">使用しません</Button>
+            <div style = {{display:"flex",marginTop:"20px",gap:"200px"}}>
+                <Button href="/setup" variant="contained"
+                style = {{width:"120px",height:"80px",fontWeight:"bold"}}>使用します</Button>
+                <Button variant="outlined" color="inherit"
+                style = {{width:"120px",height:"80px",fontWeight:"bold"}}>使用しません</Button>
+            </div>
         </div>
     );
 }
